3a04647d216261f4ace971548a71aeed737bbb62
Liudas Drejeris <drejeris.liudas@gmail.com>
2017-11-22 22:37:09 +0200

    Added translation files.

---
+++ build/CMakeFiles/com.github.drejerisliudas.movies.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/liudas/Projects/movies/build/src/main.c" "/home/liudas/Projects/movies/build/CMakeFiles/com.github.drejerisliudas.movies.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GETTEXT_PACKAGE=\"com.github.drejerisliudas.movies\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/com.github.drejerisliudas.movies.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "src/main.c"
+  "com.github.drejerisliudas.movies_valac.stamp"
+  "CMakeFiles/com.github.drejerisliudas.movies.dir/src/main.c.o"
+  "com.github.drejerisliudas.movies.pdb"
+  "com.github.drejerisliudas.movies"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.drejerisliudas.movies.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
